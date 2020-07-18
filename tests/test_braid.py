28dbcfd1296bf5aad4ab9255cee717ab7d4d3c6c
Levi Ryffel <levi.ryffel@gmail.com>
2020-07-18 12:22:39 +0200

    renamed tests

--- tests/test_braid.py
+++ tests/test_braid.py
@@ -2,7 +2,7 @@ import unittest
 from braid import Braid
 
 
-class MyTestCase(unittest.TestCase):
+class TestBraid(unittest.TestCase):
     def setUp(self):
         self.easy_braid = Braid([1])
         self.trefoil = Braid([1, 1, 1])
@@ -15,7 +15,3 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual([[1, -2, 3, -1, 2, -3]], self.trefoil.gauss_code)
         self.assertEqual([[1, -2], [-1, 2]], self.hopf_link.gauss_code)
         self.assertEqual([[1, -2, 4, -1, 3, -4, 2, -3]], self.figure8.gauss_code)
-
-
-if __name__ == '__main__':
-    unittest.main()
